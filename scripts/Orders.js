ac65c42d5ac3a1c02c1ae72747809a06d2e541bd
ChardonneDixon <chardonne.dixon@gmail.com>
2026-05-16 14:28:50 -0500

    Order.js update

--- scripts/Orders.js
+++ scripts/Orders.js
@@ -1,6 +1,4 @@
 // scripts/Orders.js
-import { placeOrder } from "./TransientState.js"
-
 export const Orders = async () => {
     const fetchResponse = await fetch("http://localhost:8088/orders")
     const orders = await fetchResponse.json()
@@ -8,8 +6,18 @@ export const Orders = async () => {
     // Map the array of objects to an array of HTML strings
     const ordersHTML = orders.map(
         (order) => {
+            // 2. Calculate the total price from the expanded objects
+            const orderPrice = order.metal.price + order.style.price + order.size.price
+
+            // 3. Format the number as currency (Optional, but highly recommended!)
+            const formattedPrice = orderPrice.toLocaleString("en-US", {
+                style: "currency",
+                currency: "USD"
+            })
+
+            // 4. Return the HTML string with the calculated price
             return `<section class="order">
-                Order #${order.id} was placed!
+                Order #${order.id} cost ${formattedPrice}
             </section>`
         }
     )
