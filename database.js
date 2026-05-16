d8c21d63360fa61b6f0d3059566d4480bc6467fe
ChardonneDixon <chardonne.dixon@gmail.com>
2026-05-16 09:56:49 -0500

    Add database.js and index.html

---
+++ database.js
@@ -0,0 +1,47 @@
+/*
+    database.js
+    This module contains all the raw data for Kneel Diamonds and 
+    the state of the current custom jewelry order.
+*/
+
+const database = {
+    metals: [
+        { id: 1, metal: "Sterling Silver", price: 12.42 },
+        { id: 2, metal: "14K Gold", price: 736.4 },
+        { id: 3, metal: "24K Gold", price: 1258.9 },
+        { id: 4, metal: "Platinum", price: 795.45 },
+        { id: 5, metal: "Palladium", price: 1241.0 }
+    ],
+    sizes: [
+        { id: 1, carets: 0.5, price: 405 },
+        { id: 2, carets: 0.75, price: 782 },
+        { id: 3, carets: 1, price: 1470 },
+        { id: 4, carets: 1.5, price: 1997 },
+        { id: 5, carets: 2, price: 3638 }
+    ],
+    styles: [
+        { id: 1, style: "Classic", price: 500 },
+        { id: 2, style: "Modern", price: 710 },
+        { id: 3, style: "Vintage", price: 965 }
+    ],
+    customOrders: [
+        // This will eventually hold the completed orders
+    ],
+    orderBuilder: {
+        // This will hold the transient state of the user's current choices
+        // e.g., metalId: 1, sizeId: 2, styleId: 3
+    }
+}
+
+// Export a function to get copies of the data so the original is not mutated
+export const getMetals = () => {
+    return database.metals.map(metal => ({...metal}))
+}
+
+export const getSizes = () => {
+    return database.sizes.map(size => ({...size}))
+}
+
+export const getStyles = () => {
+    return database.styles.map(style => ({...style}))
+}
